fb098b54a6e78c98ee725255666357bb02231147
zhouyuhao <stevenzack@qq.com>
2020-09-08 17:21:50 +0800

    Just a Backup

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"log"
+	"path/filepath"
+
+	"github.com/StevenZack/openurl"
+
 	"github.com/StevenZack/frontpage"
+	"github.com/StevenZack/tools/fileToolkit"
 	"github.com/gofaith/goctl-gui/views"
+	"github.com/gofaith/goctl/api/jsgen"
 )
 
 func main() {
 	app := frontpage.New(views.Bytes_IndexHtml, 0)
+	app.Bind(genJs)
 	app.Open()
 	app.Run()
+}
+
+func genJs(apiFile string) {
+	dir := filepath.Join(fileToolkit.GetHomeDir(), "Downloads", "js")
+	e := jsgen.JsGen(apiFile, dir)
+	if e != nil {
+		log.Println(e)
+		return
+	}
+	openurl.Open(dir)
 }
